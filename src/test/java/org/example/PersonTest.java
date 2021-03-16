33843c577b8f2200e5b603e4dca25db0e528b248
Sean Campbell <seancampbell@Seans-MBP.lan>
2021-03-16 19:25:31 -0400

    Initial commit.

---
+++ src/test/java/org/example/PersonTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.rule.impl.EqualsAndHashCodeMatchRule;
+import com.openpojo.validation.rule.impl.GetterMustExistRule;
+import com.openpojo.validation.rule.impl.SetterMustExistRule;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+import org.example.Person;
+import org.junit.Test;
+
+public class PersonTest {
+
+    @Test
+    public void test_gettersAndSetters() {
+
+        PojoClass pojoClass = PojoClassFactory
+                .getPojoClass(Person.class);
+
+        Validator validator = ValidatorBuilder
+                .create()
+                .with(new GetterMustExistRule())
+                .with(new SetterMustExistRule())
+                .with(new EqualsAndHashCodeMatchRule())
+                .with(new GetterTester())
+                .with(new SetterTester())
+                .build();
+
+        validator.validate(pojoClass);
+    }
+}
